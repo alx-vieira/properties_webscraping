7fceda432d3253d5245c4c1214234ef502aef0f3
alx-vieira <alxsv@hotmail.com>
2022-08-04 17:02:30 -0300

    Create a main function

--- src/main.js
+++ src/main.js
@@ -5,8 +5,34 @@ const linksPg = require('./linksPg');
 const urlalvo = "https://www.vivareal.com.br/venda/sp/sao-paulo/granja_comercial/#area-desde=20000";
 
 const main = async () => {
+    
+    const browser = await puppeteer.launch({
+        // headless: false
+    });
+    const page = await browser.newPage();
     const urls = await linksPg(urlalvo);
-    console.log(urls);
+    for(let i = 0; i < urls.length; ++i) {
+        let detalhesImovel = [];
+        const url = urls[i];
+        await page.goto(url);
+        await page.waitForTimeout(3000);
+        const nomeBruto = await page.$eval('.title__title', (el) => el.textContent);
+        const nome = nomeBruto.trim();
+        const valor = await page.$eval('.price__price-info', (el) => el.textContent);
+        const codigoBruto = await page.$eval('.title__code', (el) => el.textContent);
+        const codigo = codigoBruto.slice(5);
+        const urlImovel = url;
+        const detalhesArray = await page.$$eval('.features__item', li => {
+            return li.map(text => text.title +' '+ text.textContent.trim());
+        });
+
+        for (let detalhesElement of detalhesArray) {
+            detalhesImovel.push(detalhesElement);
+        }    
+        await console.log("NOME: " + nome, "VALOR: " + valor, "CODIGO: " + codigo, "URLIMOVEL: " + urlImovel, "DETALHES IMOVEL: " + detalhesImovel);
+    }
+
+    await browser.close();
 }
 
 main();
